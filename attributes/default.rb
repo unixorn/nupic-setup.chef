fad484d8507b728635f0213207536dd6ec7c33ff
Joe Block <jpb@groksolutions.com>
2013-10-28 16:35:39 -0700

    Grok is no longer using Amazon linux internally
    Since we no longer use Amazon linux, we are removing the stale list of
    Amazon linux rpms until a volunteer steps up to maintain it.

--- attributes/default.rb
+++ attributes/default.rb
@@ -58,32 +58,39 @@ when 'centos'
     'yum-utils'    => '1.1.30-14.el6',
     'zsh'          => '4.3.10-5.el6',
   }
-when 'amazon'
-  default.nupic.packages.system.nucleus = {
-    'bind-utils'   => '9.8.2-0.17.rc1.29.amzn1',
-    'cronie'       => '1.4.4-7.5.amzn1',
-    'curl'         => '7.27.0-9.32.amzn1',
-    'dash'         => '0.5.5.1-4.5.amzn1',
-    'daemonize'    => '1.7.3-1.el6',
-    'emacs'        => '23.1-21.15.amzn1',
-    'git'          => '1.8.1.4-1.27.amzn1',
-    'htop'         => '1.0.1-2.el6',
-    'iotop'        => '0.3.2-3.2.amzn1',
-    'logrotate'    => '3.7.8-16.11.amzn1',
-    'lsof'         => '4.82-4.10.amzn1',
-    'man'          => '1.6f-32.10.amzn1',
-    'ntpdate'      => '4.2.6p5-2.21.amzn1',
-    'rpm-build'    => '4.8.0-32.40.amzn1',
-    'rsync'        => '3.0.6-9.12.amzn1',
-    'screen'       => '4.0.3-16.5.amzn1',
-    'telnet'       => '0.17-47.7.amzn1',
-    'tmux'         => '1.7-1.4.amzn1',
-    'tree'         => '1.6.0-5.7.amzn1',
-    'vim-enhanced' => '7.2.411-1.8.17.amzn1',
-    'wget'         => '1.14-3.9.amzn1',
-    'yum-utils'    => '1.1.30-14.13.amzn1',
-    'zsh'          => '4.3.10-5.6.amzn1'
-  }
+#
+# Grok is no longer using Amazon linux, so we're removing this section until
+#  someone volunteers to maintain the Amazon rpm list.
+#
+# when 'amazon'
+#   default.nupic.packages.system.nucleus = {
+#     'bind-utils'   => '9.8.2-0.17.rc1.29.amzn1',
+#     'cronie'       => '1.4.4-7.5.amzn1',
+#     'curl'         => '7.27.0-9.32.amzn1',
+#     'dash'         => '0.5.5.1-4.5.amzn1',
+#     'daemonize'    => '1.7.3-1.el6',
+#     'emacs'        => '23.1-21.15.amzn1',
+#     'git'          => '1.8.1.4-1.27.amzn1',
+#     'htop'         => '1.0.1-2.el6',
+#     'iotop'        => '0.3.2-3.2.amzn1',
+#     'logrotate'    => '3.7.8-16.11.amzn1',
+#     'lsof'         => '4.82-4.10.amzn1',
+#     'man'          => '1.6f-32.10.amzn1',
+#     'mysql'        => '5.5-1.3.amzn1',
+#     'mysql-devel'  => '5.5-1.3.amzn1',
+#     'mysql-server' => '5.5-1.3.amzn1',
+#     'ntpdate'      => '4.2.6p5-2.21.amzn1',
+#     'rpm-build'    => '4.8.0-32.40.amzn1',
+#     'rsync'        => '3.0.6-9.12.amzn1',
+#     'screen'       => '4.0.3-16.5.amzn1',
+#     'telnet'       => '0.17-47.7.amzn1',
+#     'tmux'         => '1.7-1.4.amzn1',
+#     'tree'         => '1.6.0-5.7.amzn1',
+#     'vim-enhanced' => '7.2.411-1.8.17.amzn1',
+#     'wget'         => '1.14-3.9.amzn1',
+#     'yum-utils'    => '1.1.30-14.13.amzn1',
+#     'zsh'          => '4.3.10-5.6.amzn1'
+#   }
 end
 
 # By default, we are not running in Vagrant.
